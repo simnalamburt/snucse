d447df87d24063326d953d13fa52c708745b24f0
Hyeon Kim <simnalamburt@gmail.com>
2015-03-03 20:40:43 +0900

    스트라이드랑 메모리 크기 따로 조정할수있도록, 한 조건당 5회씩 테스트

--- SystemProgramming/assign1/src/main.rs
+++ SystemProgramming/assign1/src/main.rs
@@ -4,32 +4,30 @@ extern crate time;
 
 use time::PreciseTime;
 
-fn test(num: usize) {
-    let size = num*num;
+fn test(stride: usize, size: usize) {
     let a = vec![b'a'; size];
     let mut b = vec![0u8; size];
 
     let t0 = PreciseTime::now();
 
     // Sequential Access
-    let mut i = 0;
-    while i < size {
-        b[i] = a[i];
-        i += 1;
+    let mut idx: usize = 0;
+    while idx < size {
+        b[idx] = a[idx];
+        idx += 1;
     }
 
     let t1 = PreciseTime::now();
 
     // Random Access
-    let mut x = 0;
-    let mut y: usize;
-    while x < num {
-        y = 0;
-        while y < size {
-            b[x + y] = a[x + y];
-            y += num;
+    let mut base: usize = 0;
+    while base < stride {
+        let mut idx = base;
+        while idx < size {
+            b[idx] = a[idx];
+            idx += stride;
         }
-        x += 1;
+        base += 1;
     }
 
     let t2 = PreciseTime::now();
@@ -38,10 +36,16 @@ fn test(num: usize) {
     let unfriendly = t1.to(t2).num_nanoseconds().unwrap();
     let ratio = friendly as f64/unfriendly as f64;
 
-    println!("{:>5}, {:>10}, {:>20}, {:>20}, {:>20}", num, size, friendly, unfriendly, ratio);
+    println!("{:>5}, {:>10}, {:>20}, {:>20}, {:>20}", stride, size, friendly, unfriendly, ratio);
 }
 
 fn main() {
-    println!("{:>5}, {:>10}, {:>20}, {:>20}, {:>20}", "#", "size", "friendly", "unfriendly", "unfriendly / friendly");
-    for num in 1usize..10000 { test(num) }
+    println!("{:>5}, {:>10}, {:>20}, {:>20}, {:>20}", "stride", "size", "friendly", "unfriendly", "unfriendly / friendly");
+    for stride in 1usize..64 {
+        for exp in 11..25 {
+            for _ in 0..5 {
+                test(stride, 2 << exp)
+            }
+        }
+    }
 }
