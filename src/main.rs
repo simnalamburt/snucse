9b367ac3773421ac0b3011f8ce323d806324daf6
Hyeon Kim <simnalamburt@gmail.com>
2015-03-08 21:28:56 +0900

    Organized codes

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,26 @@ extern crate time;
 
 use time::PreciseTime;
 
+fn main() {
+    println!("size,t");
+    for n in 10..30 {
+        test(n)
+    }
+}
+
 fn test(exp: usize) {
+    let stride = 16;
     let size = 2 << exp;
-    let mask = size - 1;
     let mut arr = vec![0u8; size];
 
+    let mask = size - 1; // x & mask == x % arr.len()
     let t0 = PreciseTime::now();
 
     for i in 0usize..64*1024*1024 {
-        arr[(i * 16) & mask] += 1;
-        // x & lengthMod == x % arr.Length
+        arr[(i * stride) & mask] += 1;
     }
 
     let t1 = PreciseTime::now();
     let t = t0.to(t1).num_nanoseconds().unwrap();
-    println!("{:>15}, {:>15}", size, t);
-}
-
-fn main() {
-    println!("{:>15}, {:>15}", "size", "t");
-    for n in 10..30 { test(n) }
+    println!("{},{}", size, t);
 }
