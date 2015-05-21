bd7a3f76964dcc96c369be40c6c8d8020ebf9189
Hyeon Kim <simnalamburt@gmail.com>
2015-05-21 13:14:06 +0900

    시프 과제

---
+++ System Programming/sync/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(std_misc)]
+
+use std::sync::{Arc, Semaphore};
+use std::thread;
+
+static mut var: i32 = 1;
+
+fn main() {
+    let sem = Arc::new(Semaphore::new(1));
+
+    let n = 8;
+
+    let mut threads = Vec::new();
+    for _ in 0..n {
+        let sem = sem.clone();
+        threads.push(thread::spawn(move || {
+            for _ in 0..1000_0000/n {
+                let _guard = sem.access();
+                unsafe { var += 1 }
+            }
+        }))
+    }
+
+    for thread in threads.into_iter() {
+        let _ = thread.join();
+    }
+
+    unsafe {
+        println!("var : {}", var);
+    }
+}
