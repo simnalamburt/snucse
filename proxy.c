567827bd2341a22f9eb6cb215840d44e0eb43fb3
Hyeon Kim <simnalamburt@gmail.com>
2015-06-06 14:46:33 +0900

    Indent

--- proxy.c
+++ proxy.c
@@ -23,13 +23,13 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, 
 int main(int argc, char **argv)
 {
 
-    /* Check arguments */
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
-	exit(0);
-    }
-
+  /* Check arguments */
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
     exit(0);
+  }
+
+  exit(0);
 }
 
 
@@ -43,39 +43,39 @@ int main(int argc, char **argv)
  */
 int parse_uri(char *uri, char *hostname, char *pathname, int *port)
 {
-    char *hostbegin;
-    char *hostend;
-    char *pathbegin;
-    int len;
+  char *hostbegin;
+  char *hostend;
+  char *pathbegin;
+  int len;
 
-    if (strncasecmp(uri, "http://", 7) != 0) {
-	hostname[0] = '\0';
-	return -1;
-    }
+  if (strncasecmp(uri, "http://", 7) != 0) {
+    hostname[0] = '\0';
+    return -1;
+  }
 
-    /* Extract the host name */
-    hostbegin = uri + 7;
-    hostend = strpbrk(hostbegin, " :/\r\n\0");
-    len = hostend - hostbegin;
-    strncpy(hostname, hostbegin, len);
-    hostname[len] = '\0';
+  /* Extract the host name */
+  hostbegin = uri + 7;
+  hostend = strpbrk(hostbegin, " :/\r\n\0");
+  len = hostend - hostbegin;
+  strncpy(hostname, hostbegin, len);
+  hostname[len] = '\0';
 
-    /* Extract the port number */
-    *port = 80; /* default */
-    if (*hostend == ':')
-	*port = atoi(hostend + 1);
+  /* Extract the port number */
+  *port = 80; /* default */
+  if (*hostend == ':')
+    *port = atoi(hostend + 1);
 
-    /* Extract the path */
-    pathbegin = strchr(hostbegin, '/');
-    if (pathbegin == NULL) {
-	pathname[0] = '\0';
-    }
-    else {
-	pathbegin++;
-	strcpy(pathname, pathbegin);
-    }
+  /* Extract the path */
+  pathbegin = strchr(hostbegin, '/');
+  if (pathbegin == NULL) {
+    pathname[0] = '\0';
+  }
+  else {
+    pathbegin++;
+    strcpy(pathname, pathbegin);
+  }
 
-    return 0;
+  return 0;
 }
 
 /*
@@ -86,30 +86,30 @@ int parse_uri(char *uri, char *hostname, char *pathname, int *port)
  * of the response from the server (size).
  */
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr,
-		      char *uri, int size)
+    char *uri, int size)
 {
-    time_t now;
-    char time_str[MAXLINE];
-    unsigned long host;
-    unsigned char a, b, c, d;
+  time_t now;
+  char time_str[MAXLINE];
+  unsigned long host;
+  unsigned char a, b, c, d;
 
-    /* Get a formatted time string */
-    now = time(NULL);
-    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+  /* Get a formatted time string */
+  now = time(NULL);
+  strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
 
-    /*
-     * Convert the IP address in network byte order to dotted decimal
-     * form. Note that we could have used inet_ntoa, but chose not to
-     * because inet_ntoa is a Class 3 thread unsafe function that
-     * returns a pointer to a static variable (Ch 13, CS:APP).
-     */
-    host = ntohl(sockaddr->sin_addr.s_addr);
-    a = host >> 24;
-    b = (host >> 16) & 0xff;
-    c = (host >> 8) & 0xff;
-    d = host & 0xff;
+  /*
+   * Convert the IP address in network byte order to dotted decimal
+   * form. Note that we could have used inet_ntoa, but chose not to
+   * because inet_ntoa is a Class 3 thread unsafe function that
+   * returns a pointer to a static variable (Ch 13, CS:APP).
+   */
+  host = ntohl(sockaddr->sin_addr.s_addr);
+  a = host >> 24;
+  b = (host >> 16) & 0xff;
+  c = (host >> 8) & 0xff;
+  d = host & 0xff;
 
 
-    /* Return the formatted log entry string */
-    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
+  /* Return the formatted log entry string */
+  sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
 }
