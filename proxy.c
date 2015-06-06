ac43bda00b1f2713dbd695dc6497c08ca8396639
Hyeon Kim <simnalamburt@gmail.com>
2015-06-07 05:22:09 +0900

    Unused header

--- proxy.c
+++ proxy.c
@@ -27,13 +27,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
