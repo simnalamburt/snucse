7dc094af47229059bdba7e84f4288e77d2965b81
Hyeon Kim <simnalamburt@gmail.com>
2015-06-19 08:22:37 +0900

    Task들 병렬로 수행

--- main.cc
+++ main.cc
@@ -26,22 +26,45 @@ namespace {
   };
 
   struct task_t {
-    int Id;
-    double pdYield[N];
-    double ppdFactors[FACTORS][N - 1];
     double result_mean;
     double result_error;
   };
 
   task_t tasks[TASKS];
 
-  void *worker(void *arg) {
-    param_t *param = (param_t*)arg;
+  //
+  // Constants
+  //
+  const double factors[FACTORS][N - 1] = {
+    { 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01 },
+    { 0.009048, 0.008187, 0.007408, 0.006703, 0.006065, 0.005488, 0.004966, 0.004493, 0.004066, 0.003679 },
+    { 0.001000, 0.000750, 0.000500, 0.000250, 0.000000, -0.000250, -0.000500, -0.000750, -0.001000, -0.001250 }
+  };
 
+  void *worker(void *arg) {
     auto time = system_clock::now();
+
+    param_t *param = (param_t*)arg;
     for (int i = param->begin; i < param->end; ++i) {
+      //
+      // Initialize
+      //
+      double pdYield[N];
+      double tmp = 0.1;
+      for (int j = 0; j < N; ++j) {
+        pdYield[j] = tmp;
+        tmp += 0.005;
+      }
+
+      double ppdFactors[FACTORS][N - 1];
+      memcpy(ppdFactors, factors, sizeof factors);
+
+
+      //
+      // Calculate
+      //
       double result[2];
-      swaption(result, (double)tasks[i].Id/(double)TASKS, tasks[i].pdYield, tasks[i].ppdFactors);
+      swaption(result, (double)i/(double)TASKS, pdYield, ppdFactors);
       tasks[i].result_mean = result[0];
       tasks[i].result_error = result[1];
     }
@@ -56,31 +79,6 @@ namespace {
 int main() {
   auto time = system_clock::now();
 
-  //
-  // Initialize input dataset
-  //
-  const double factors[FACTORS][N - 1] = {
-    { 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01 },
-    { 0.009048, 0.008187, 0.007408, 0.006703, 0.006065, 0.005488, 0.004966, 0.004493, 0.004066, 0.003679 },
-    { 0.001000, 0.000750, 0.000500, 0.000250, 0.000000, -0.000250, -0.000500, -0.000750, -0.001000, -0.001250 }
-  };
-
-  for (int i = 0; i < TASKS; i++) {
-    tasks[i].Id = i;
-
-    double tmp = 0.1;
-    for (int j = 0; j < N; ++j) {
-      tasks[i].pdYield[j] = tmp;
-      tmp += 0.005;
-    }
-
-    memcpy(tasks[i].ppdFactors, factors, sizeof factors);
-  }
-
-
-  //
-  // Calling the Swaption Pricing Routine
-  //
   param_t params[THREADS];
   const int chunksize = TASKS/THREADS;
   for (int i = 0; i < THREADS; ++i) {
@@ -95,10 +93,9 @@ int main() {
 
   for (int i = 0; i < THREADS; ++i) {
     pthread_join(threads[i], NULL);
-  }
-
-  for (int i = 0; i < TASKS; ++i) {
-    printf("Swaption%d: [SwaptionPrice: %.10lf StdError: %.10lf]\n", i, tasks[i].result_mean, tasks[i].result_error);
+    for (int n = params[i].begin; n < params[i].end; ++n) {
+      printf("Swaption%d: [SwaptionPrice: %.10lf StdError: %.10lf]\n", n, tasks[n].result_mean, tasks[n].result_error);
+    }
   }
 
   auto elapsed = duration<double>(system_clock::now() - time).count();
