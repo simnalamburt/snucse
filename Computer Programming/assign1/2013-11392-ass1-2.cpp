f0886e734f25b99668ff0766a17716103d7cc1c2
Hyeon Kim <simnalamburt@gmail.com>
2015-09-28 18:09:00 +0900

    주석 추가

--- Computer Programming/assign1/2013-11392-ass1-2.cpp
+++ Computer Programming/assign1/2013-11392-ass1-2.cpp
@@ -1,19 +1,36 @@
+//
+// 컴퓨터 프로그래밍, 과제 1번 연습문제 2번.
+//
+// C++11 문법이 허용되는지 여부를 몰라서 C++03 문법으로 코딩하였음
+//
+
 #include <iostream>
 #include <string>
 #include <cassert>
 
 using namespace std;
 
+//
+// 유저가 다음 개행문자('\n')를 입력할때까지 쓴 입력을 모두 받음.
+// 개행문자는 결과에 포함되지 않음.
+//
 string getline() {
   string line;
   ::std::getline(cin, line);
   return line;
 }
 
+//
+// {a~z}, {A~Z}, {1~9} 로 이뤄진 문자열의 각 글자들을 [0, 61) 범위의 숫자로 1:1
+// 대응시킨다.
+//
+// 범위 밖의 입력이 들어올경우, std::out_of_range 예외를 발생시킨다.
+//
 void numerify(string& input) {
   string::iterator i = input.begin();
   string::const_iterator end = input.cend();
 
+  // 이터레이터로 각 글자에 대해 모두 실행함
   for (; i != end; ++i) {
     uint8_t ch = *i;
 
@@ -33,6 +50,11 @@ void numerify(string& input) {
   }
 }
 
+//
+// numerify() 함수의 역함수
+//
+// 범위 밖의 입력이 들어올경우, std::out_of_range 예외를 발생시킨다.
+//
 void stringify(string& input) {
   string::iterator i = input.begin();
   string::const_iterator end = input.cend();
@@ -54,13 +76,21 @@ void stringify(string& input) {
   }
 }
 
+//
+// 프로그램 본체
+//
 int main() {
+  //
+  // 터미널 컬러코드
+  //
   const char *blue = "\e[34m";
   const char *orange = "\e[33m";
   const char *green = "\e[32m";
   const char *reset = "\e[0m";
 
   while (true) {
+    // Encrypt인지, Decrypt인지 입력받는다.
+    // 반복문을 써서, 잘못된 입력이 들어올경우 올바른 입력이 주어질때까지 반복한다
     bool encrypt;
     while (true) {
       try {
@@ -75,6 +105,7 @@ int main() {
         encrypt = mode == 1;
         break;
       } catch(invalid_argument) {
+        // EOF가 주어질경우 프로그램을 꺼버린다.
         if (cin.eof()) { cout << endl; return 0; }
         cout << orange << "Please choose between '1' and '2'" << reset << endl;
       } catch(out_of_range e) {
@@ -82,7 +113,8 @@ int main() {
       }
     }
 
-
+    // 시저 암호를 적용시킬 문자열을 입력받는다.
+    // 반복문을 써서, 잘못된 입력이 들어올경우 올바른 입력이 주어질때까지 반복한다
     string line;
     while (true) {
       try {
@@ -96,7 +128,7 @@ int main() {
       }
     }
 
-
+    // 키를 입력받는다
     int64_t key;
     while (true) {
       try {
@@ -110,7 +142,7 @@ int main() {
       }
     }
 
-
+    // 문자열의 각 글자에 대해 시저암호를 계산한다
     string::const_iterator end = line.cend();
     for (string::iterator i = line.begin(); i != end; ++i) {
       uint8_t num = *i;
@@ -122,7 +154,7 @@ int main() {
       *i = after;
     }
 
-
+    // 숫자로 변환된 문자열을 다시 문자열로 바꾼다
     stringify(line);
     cout << '\n' << green << line << reset << "\n\n\n" << flush;
   }
