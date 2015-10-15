79fb335fa52235cde7620121ba6e079e523b65b1
Hyeon Kim <simnalamburt@gmail.com>
2015-10-15 20:23:55 +0900

    PCA 적용, 시간 표시

--- code_2013_11392_HyeonKim/learn.py
+++ code_2013_11392_HyeonKim/learn.py
@@ -1,9 +1,16 @@
-import gzip, os, time, cPickle
-import numpy as np
-import tool
-from scipy.cluster.vq import kmeans, vq
+# -*- coding: utf-8 -*-
+from time import time
+t0 = time()
 
-start_time = time.time()
+#
+# Loading
+#
+print 'numpy, 데이터 로딩',
+
+import gzip, os, cPickle
+import tool
+import numpy as np
+from scipy.cluster.vq import kmeans, vq
 
 path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
 with gzip.open(path, 'rb') as f:
@@ -12,10 +19,13 @@ with gzip.open(path, 'rb') as f:
 train_count = 5000
 train_x, train_y = (train_set[0][:train_count], train_set[1][:train_count])
 
+t1 = time()
+print '완료 (%f seconds)' % (t1 - t0)
 
 #
 # PCA
 #
+print 'PCA 수행',
 dimension = 100
 _, vec = np.linalg.eig(np.cov(train_x.transpose()))
 vec = np.real(vec)
@@ -24,10 +34,13 @@ pca = vec.transpose()[:dimension]
 train_x = pca.dot(train_x.transpose()).transpose()
 train_count = dimension
 
+t2 = time()
+print '완료 (%f seconds)' % (t2 - t1)
 
 #
 # Clustering
 #
+print 'K-means 클러스터링',
 K = min(train_count, 250)
 centroids, _ = kmeans(train_x, K)
 groups, _ = vq(train_x, centroids)
@@ -35,20 +48,27 @@ groups, _ = vq(train_x, centroids)
 mu      = np.zeros((K, train_x.shape[1]))
 sigma   = np.zeros((K, train_x.shape[1], train_x.shape[1]))
 
+t3 = time()
+print '완료 (%f seconds)' % (t3 - t2)
 
 #
 # Training
 #
+print '평균, 표준편차, Weight 행렬 계산',
 for j in range(K):
     selected = train_x[groups[j]]
     mu[j] = sum(selected)/selected.shape[0]
     sigma[j] = np.cov(selected.transpose()) + 0.1 * np.eye(train_x.shape[1])
-
 
 H = tool.get_H(train_x, mu, sigma)
 Ht = H.transpose()
 Y = tool.get_Y(train_y)
 W = np.linalg.lstsq(Ht.dot(H), Ht.dot(Y))[0]
 
-elapsed_time = time.time() - start_time
-print 'Elapsed Time for Training: ', elapsed_time
+tEnd = time()
+print '완료 (%f seconds)' % (tEnd - t3)
+
+
+print ''
+print '학습에 쓰인 총 시간: %f seconds' % (tEnd - t0)
+print ''
--- code_2013_11392_HyeonKim/run_rbfn.py
+++ code_2013_11392_HyeonKim/run_rbfn.py
@@ -1,19 +1,35 @@
-import gzip, os, time, cPickle
-import numpy as np
+# -*- coding: utf-8 -*-
 import learn
+import gzip, os, cPickle
+import numpy as np
 import tool
+from time import time
 
-start_time = time.time()
+t0 = time()
+
+#
+# Loading
+#
+print '테스트 데이터 로딩 및 PCA 적용',
 
 path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
 with gzip.open(path, 'rb') as f:
     _, _, test_set = cPickle.load(f)
 
+test_count = 2000
+test_x, test_y = (test_set[0][:test_count], test_set[1][:test_count])
+
+# PCA
+test_x = learn.pca.dot(test_x.transpose()).transpose()
+test_count = learn.dimension
+
+t1 = time()
+print '완료 (%f seconds)' % (t1 - t0)
+
 #
 # Test
 #
-test_count = 2000
-test_x, test_y = (test_set[0][:test_count], test_set[1][:test_count])
+print '퍼포먼스',
 
 H = tool.get_H(test_x, learn.mu, learn.sigma)
 Y = tool.get_Y(test_y)
@@ -21,15 +37,25 @@ f = H.dot(learn.W)
 max_val = f.max(1)
 max_idx = f.argmax(1)
 
+t2 = time()
+print '완료 (%f seconds)' % (t2 - t1)
 
-# row: test_label
-# col: estimate of the classifier
+#
+# Grade
+#
+print '채점',
+
 confusion_matrix = np.zeros((10,10))
 for i in range(test_y.shape[0]):
     confusion_matrix[test_y[i], max_idx[i]] += 1
 
 accuracy = np.diag(confusion_matrix).sum() / test_y.shape[0]
-elapsed_time = time.time() - start_time
 
-print 'Elapsed Time for Testing: ', elapsed_time
-print 'Accuracy: ', accuracy
+t3 = time()
+print '완료 (%f seconds)' % (t3 - t2)
+
+
+print ''
+print '퍼포먼스에 쓰인 총 시간: %f seconds' % (t3 - t0)
+print '정확도: %f%%' % (accuracy * 100.0)
+print ''
