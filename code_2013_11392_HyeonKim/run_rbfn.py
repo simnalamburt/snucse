f51a92fa1f1313908a9ba3dec0905af6e116cf24
Hyeon Kim <simnalamburt@gmail.com>
2015-10-15 12:12:22 +0900

    리니어 리그레션

--- code_2013_11392_HyeonKim/run_rbfn.py
+++ code_2013_11392_HyeonKim/run_rbfn.py
@@ -7,41 +7,54 @@ path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
 with gzip.open(path, 'rb') as f:
     train_set, valid_set, test_set = cPickle.load(f)
 
-train_count = 1000
-test_count = 100
+train_count = 10000
+test_count = 200
 
 train_x, train_y = (train_set[0][:train_count], train_set[1][:train_count])
-test_x, test_y = (test_set[0][:test_count], test_set[1][:test_count])
 
-p_class     = np.zeros(10)
-train_subx  = [None] * 10
-mu          = [None] * 10
-sigma       = [None] * 10
+probability = np.zeros(10)
+mu          = np.zeros((10, train_x.shape[1]))
+sigma       = np.zeros((10, train_x.shape[1], train_x.shape[1]))
 
 #
 # Training
 #
 for j in range(10):
-    train_subx[j] = train_x[train_y==j];
-    mu[j] = sum(train_subx[j])/train_subx[j].shape[0]
-    sigma[j] = np.cov(train_subx[j].transpose()) + 0.1 * np.eye(train_x.shape[1])
-    p_class[j] = 1.0 * train_subx[j].shape[0] / train_x[j].shape[0]
+    selected = train_x[train_y==j]
+    probability[j] = 1.0 * selected.shape[0] / train_x[j].shape[0]
+    mu[j] = sum(selected)/selected.shape[0]
+    sigma[j] = np.cov(selected.transpose()) + 0.1 * np.eye(train_x.shape[1])
 
-# row: test_label
-# col: estimate of the classifier
-confusion_matrix = np.zeros((10,10));
+def get_hidden(X):
+    hidden = np.zeros((X.shape[0], 10))
+    for j in range(10):
+        hidden[:,j] = mvn.pdf(X, mu[j], sigma[j])
+    return hidden
 
-p_likelihood = np.zeros((10, test_x.shape[0]));
+def get_Y(y):
+    return np.eye(y.shape[0])[y][:,:10]
 
-for j in range(10):
-    p_likelihood[j] = p_class[j] * mvn.pdf(test_x, mu[j], sigma[j]);
+hidden = get_hidden(train_x)
+hidden_t = hidden.transpose()
+Y = get_Y(train_y)
+W = np.linalg.inv(hidden_t.dot(hidden)).dot(hidden_t).dot(Y) # <- opt
 
-max_val = p_likelihood.max(0)
-max_idx = p_likelihood.argmax(0)
 
 #
 # Test
 #
+test_x, test_y = (test_set[0][:test_count], test_set[1][:test_count])
+
+hidden = get_hidden(test_x)
+Y = get_Y(test_y)
+f = Y.dot(W)
+max_val = f.max(1)
+max_idx = f.argmax(1)
+
+
+# row: test_label
+# col: estimate of the classifier
+confusion_matrix = np.zeros((10,10))
 for i in range(test_y.shape[0]):
     confusion_matrix[test_y[i], max_idx[i]] += 1
 
