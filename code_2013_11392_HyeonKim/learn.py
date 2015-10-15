0433cdfaea75d30fb8ec4d1b967f52cb3644dce0
Hyeon Kim <simnalamburt@gmail.com>
2015-10-15 13:12:22 +0900

    학습이랑 테스트 분리, 92%

---
+++ code_2013_11392_HyeonKim/learn.py
@@ -0,0 +1,30 @@
+import gzip, os, cPickle
+import numpy as np
+import tool
+
+path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
+with gzip.open(path, 'rb') as f:
+    train_set, _, _ = cPickle.load(f)
+
+train_count = 10000
+train_x, train_y = (train_set[0][:train_count], train_set[1][:train_count])
+
+probability = np.zeros(10)
+mu          = np.zeros((10, train_x.shape[1]))
+sigma       = np.zeros((10, train_x.shape[1], train_x.shape[1]))
+
+
+#
+# Training
+#
+for j in range(10):
+    selected = train_x[train_y==j]
+    probability[j] = 1.0 * selected.shape[0] / train_x[j].shape[0]
+    mu[j] = sum(selected)/selected.shape[0]
+    sigma[j] = np.cov(selected.transpose()) + 0.1 * np.eye(train_x.shape[1])
+
+
+H = tool.get_H(train_x, mu, sigma)
+Ht = H.transpose()
+Y = tool.get_Y(train_y)
+W = np.linalg.inv(Ht.dot(H)).dot(Ht).dot(Y) # <- opt
--- code_2013_11392_HyeonKim/run_rbfn.py
+++ code_2013_11392_HyeonKim/run_rbfn.py
@@ -1,53 +1,21 @@
-#!/usr/bin/env python2
 import gzip, os, cPickle
 import numpy as np
-from scipy.stats import multivariate_normal as mvn
+import learn
+import tool
 
 path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
 with gzip.open(path, 'rb') as f:
-    train_set, valid_set, test_set = cPickle.load(f)
-
-train_count = 10000
-test_count = 200
-
-train_x, train_y = (train_set[0][:train_count], train_set[1][:train_count])
-
-probability = np.zeros(10)
-mu          = np.zeros((10, train_x.shape[1]))
-sigma       = np.zeros((10, train_x.shape[1], train_x.shape[1]))
-
-#
-# Training
-#
-for j in range(10):
-    selected = train_x[train_y==j]
-    probability[j] = 1.0 * selected.shape[0] / train_x[j].shape[0]
-    mu[j] = sum(selected)/selected.shape[0]
-    sigma[j] = np.cov(selected.transpose()) + 0.1 * np.eye(train_x.shape[1])
-
-def get_hidden(X):
-    hidden = np.zeros((X.shape[0], 10))
-    for j in range(10):
-        hidden[:,j] = mvn.pdf(X, mu[j], sigma[j])
-    return hidden
-
-def get_Y(y):
-    return np.eye(y.shape[0])[y][:,:10]
-
-hidden = get_hidden(train_x)
-hidden_t = hidden.transpose()
-Y = get_Y(train_y)
-W = np.linalg.inv(hidden_t.dot(hidden)).dot(hidden_t).dot(Y) # <- opt
-
+    _, _, test_set = cPickle.load(f)
 
 #
 # Test
 #
+test_count = 2000
 test_x, test_y = (test_set[0][:test_count], test_set[1][:test_count])
 
-hidden = get_hidden(test_x)
-Y = get_Y(test_y)
-f = Y.dot(W)
+H = tool.get_H(test_x, learn.mu, learn.sigma)
+Y = tool.get_Y(test_y)
+f = H.dot(learn.W)
 max_val = f.max(1)
 max_idx = f.argmax(1)
 
