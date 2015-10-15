477f341e0a29b2094005c5fe19c706cf55988ceb
Hyeon Kim <simnalamburt@gmail.com>
2015-10-15 15:48:53 +0900

    K-means

--- code_2013_11392_HyeonKim/learn.py
+++ code_2013_11392_HyeonKim/learn.py
@@ -1,23 +1,29 @@
 import gzip, os, cPickle
 import numpy as np
 import tool
+from scipy.cluster.vq import kmeans, vq
 
 path = os.path.join(os.path.realpath('..'), "data", 'mnist.pkl.gz')
 with gzip.open(path, 'rb') as f:
     train_set, _, _ = cPickle.load(f)
 
-train_count = 10000
+train_count = 100
+K = min(train_count, 1000)
+
 train_x, train_y = (train_set[0][:train_count], train_set[1][:train_count])
 
-mu          = np.zeros((10, train_x.shape[1]))
-sigma       = np.zeros((10, train_x.shape[1], train_x.shape[1]))
+centroids, _ = kmeans(train_x, K)
+groups, _ = vq(train_x, centroids)
+
+mu      = np.zeros((K, train_x.shape[1]))
+sigma   = np.zeros((K, train_x.shape[1], train_x.shape[1]))
 
 
 #
 # Training
 #
-for j in range(10):
-    selected = train_x[train_y==j]
+for j in range(K):
+    selected = train_x[groups[j]]
     mu[j] = sum(selected)/selected.shape[0]
     sigma[j] = np.cov(selected.transpose()) + 0.1 * np.eye(train_x.shape[1])
 
