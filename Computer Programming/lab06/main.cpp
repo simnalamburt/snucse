3ec9c98e221f7b2a604252c7afde6dfcd7be2637
Hyeon Kim <simnalamburt@gmail.com>
2015-10-19 02:47:02 +0900

    주석 추가

--- Computer Programming/lab06/main.cpp
+++ Computer Programming/lab06/main.cpp
@@ -18,14 +18,18 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // [(100, 'a'), (50, 'b'), ... ]
   array<entry, 256> freq = {};
 
+  // 배열 초기화
   char ch = 0;
   for (auto& entry: freq) {
     entry.first = 0;
     entry.second = ch++;
   }
+  // [(0, 'a'), (0, 'b'), ... ]
 
+  // 입력에서 읽어서 배열에 넣음
   while (true) {
     char ch = input.get();
     if (input.eof()) { break; }
@@ -33,19 +37,23 @@ int main(int argc, char **argv) {
   }
   input.close();
 
+  // 빈도순으로 정렬함
   sort(freq.begin(), freq.end(), [](entry left, entry right) {
     return left.first > right.first;
   });
 
+  // 출력파일 열기
   ofstream output(argv[2]);
   if (!output.is_open()) {
     cerr << "Unable to open file to write" << endl;
     return 1;
   }
 
+  // 배열 순회하면서 출력
   for (auto entry : freq) {
     size_t count = entry.first;
     char ch = entry.second;
+    // 0회일경우, 스킵
     if (count != 0) {
       output << ch << "(value : " << size_t(ch) << "): " << count << endl;
     }
