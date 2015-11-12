5912db69d39e1aab001dcaf2dab4fa5db7ab68d7
Hyeon Kim <simnalamburt@gmail.com>
2015-11-13 08:32:40 +0900

    컴프: 빈 main() 함수 작성

---
+++ Computer Programming/assign2/2013-11392-assign2-1-c11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Container {
+  enum class ContainerType { normal, protect, cold };
+
+};
+
+class Item {
+  enum class ItemType { normal, flammable, ice };
+
+};
+
+int main() {
+  return 0;
+}
