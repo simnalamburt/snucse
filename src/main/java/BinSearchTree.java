5a6dbe0f2ac681abb0b9aea96054330350be5946
Hyeon Kim <simnalamburt@gmail.com>
2015-12-08 18:55:45 +0900

    인서트 구현

--- src/main/java/BinSearchTree.java
+++ src/main/java/BinSearchTree.java
@@ -1,26 +1,49 @@
 public class BinSearchTree {
-    private Node root;
-
-    public BinSearchTree() {
-        root = null;
-    }
-
     private class Node {
-        private Node left;
-        private Node right;
+        private Node left, right;
         private int data;
+
         Node(int data) {
-            right = null;
-            left = null;
+            right = left = null;
             this.data = data;
+        }
+
+        void insert(int child) {
+            // 낮으면 왼쪽, 같거나 크면 오른쪽
+            if (child < data) {
+                if (left == null) {
+                    left = new Node(child);
+                    return;
+                }
+
+                left.insert(child);
+            } else {
+                if (right == null) {
+                    right = new Node(child);
+                    return;
+                }
+
+                right.insert(child);
+            }
         }
     }
 
-    public void insert(int data) {
-        // TODO: insert node
+    private Node root;
+    public BinSearchTree() { root = null; }
+
+    public void insert(int child) {
+        // On first insertion
+        if (root == null) {
+            root = new Node(child);
+            return;
+        }
+
+        root.insert(child);
     }
 
     public void delete(int data) {
+        // 없는걸 delete하진 않음
+
         // TODO: delete target node
     }
     public String search(int data) {
