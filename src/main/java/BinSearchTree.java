9fc9afb7e671e464aa864ffd259fab93f57235e3
Hyeon Kim <simnalamburt@gmail.com>
2015-12-08 19:31:57 +0900

    딜리트 안좋게 구현

--- src/main/java/BinSearchTree.java
+++ src/main/java/BinSearchTree.java
@@ -1,11 +1,26 @@
 public class BinSearchTree {
     private class Node {
         private Node left, right;
-        private int data;
+        public int data;
 
         Node(int data) {
             right = left = null;
             this.data = data;
+        }
+
+        boolean isLeaf() { return left == null && right == null; }
+
+        Node onlySon() {
+            if (left == null && right == null) { return null; }
+            if (left != null && right != null) { return null; }
+
+            return left != null ? left : right;
+        }
+
+        // Postcondition: returned Node is not null
+        Node smallestLeaf() {
+            if (left == null) { return this; }
+            return left.smallestLeaf();
         }
 
         void insert(int child) {
@@ -26,6 +41,37 @@ public class BinSearchTree {
                 right.insert(child);
             }
         }
+
+        // Precondition: data != query
+        void delete(int query) {
+            Node selected = query < data ? left : right;
+
+            if (_delete_check(selected, query)) { return; }
+            // selected.data == query; selected 노드를 지우기
+
+            // Case 1: selected.children = []
+            if (selected.isLeaf()) {
+                if (query < data) { left = null; } else { right = null; }
+            }
+
+            // Case 2: selected.children = [x]
+            Node onlyson = selected.onlySon();
+            if (onlyson != null) {
+                if (query < data) { left = onlyson; } else { right = onlyson; }
+            }
+
+            // Case 3: selected.children = [left, right]
+            Node smallest = selected.right.smallestLeaf();
+            selected.data = smallest.data;
+            selected.right.delete(query);
+        }
+    }
+
+    // Postcondition: node.data == query
+    static boolean _delete_check(Node node, int query) {
+        if (node == null) { return true; }
+        if (node.data != query) { node.delete(query); return true; }
+        return false;
     }
 
     private Node root;
@@ -41,11 +87,29 @@ public class BinSearchTree {
         root.insert(child);
     }
 
-    public void delete(int data) {
-        // 없는걸 delete하진 않음
+    public void delete(int query) {
+        // 트리에 없는 숫자를 delete하라고 하진 않는다고 가정되어있음
 
-        // TODO: delete target node
+        if (_delete_check(root, query)) { return; }
+        // root.data == query; root 노드 지우기
+
+        // Case 1: root.children = []
+        if (root.isLeaf()) {
+            root = null;
+        }
+
+        // Case 2: root.children = [x]
+        Node onlyson = root.onlySon();
+        if (onlyson != null) {
+            root = onlyson;
+        }
+
+        // Case 3: root.children = [left, right]
+        Node smallest = root.right.smallestLeaf();
+        root.data = smallest.data;
+        root.right.delete(query);
     }
+
     public String search(int data) {
         // TODO: if found node, return the step
         // TODO: if not found, return NONE
