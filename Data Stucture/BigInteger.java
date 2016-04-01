09e11c41e024859d3b6ed64fa70e4978cdbae209
Hyeon Kim <simnalamburt@gmail.com>
2016-04-01 13:14:59 +0900

    Remove CR

--- Data Stucture/BigInteger.java
+++ Data Stucture/BigInteger.java
@@ -2,59 +2,59 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
- 
- 
+
+
 public class BigInteger
 {
     public static final String QUIT_COMMAND = "quit";
     public static final String MSG_INVALID_INPUT = "입력이 잘못되었습니다.";
- 
+
     // implement this
     public static final Pattern EXPRESSION_PATTERN = Pattern.compile("");
- 
- 
+
+
     public BigInteger(int i)
     {
     }
- 
+
     public BigInteger(int[] num1)
     {
     }
- 
+
     public BigInteger(String s)
     {
     }
- 
+
     public BigInteger add(BigInteger big)
     {
     }
- 
+
     public BigInteger subtract(BigInteger big)
     {
     }
- 
+
     public BigInteger multiply(BigInteger big)
     {
     }
- 
+
     @Override
     public String toString()
     {
     }
- 
+
     static BigInteger evaluate(String input) throws IllegalArgumentException
     {
         // implement here
         // parse input
         // using regex is allowed
- 
+
         // One possible implementation
         // BigInteger num1 = new BigInteger(arg1);
         // BigInteger num2 = new BigInteger(arg2);
         // BigInteger result = num1.add(num2);
         // return result;
     }
- 
+
     public static void main(String[] args) throws Exception
     {
         try (InputStreamReader isr = new InputStreamReader(System.in))
@@ -65,7 +65,7 @@ public class BigInteger
                 while (!done)
                 {
                     String input = reader.readLine();
- 
+
                     try
                     {
                         done = processInput(input);
@@ -78,11 +78,11 @@ public class BigInteger
             }
         }
     }
- 
+
     static boolean processInput(String input) throws IllegalArgumentException
     {
         boolean quit = isQuitCmd(input);
- 
+
         if (quit)
         {
             return true;
@@ -91,11 +91,11 @@ public class BigInteger
         {
             BigInteger result = evaluate(input);
             System.out.println(result.toString());
- 
+
             return false;
         }
     }
- 
+
     static boolean isQuitCmd(String input)
     {
         return input.equalsIgnoreCase(QUIT_COMMAND);
