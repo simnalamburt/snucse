9ed40f4bea683dabe09fecae2c2b0df15f4df59c
Hyeon Kim <simnalamburt@gmail.com>
2016-04-15 20:58:09 +0900

    MySortedSet#delete 메서드 구현

--- Data Stucture/Movie Database/MySortedSet.java
+++ Data Stucture/Movie Database/MySortedSet.java
@@ -1,7 +1,7 @@
 public class MySortedSet<E extends Comparable<E>> extends MyLinkedList<E> {
     @Override
     public void add(E elem) {
-        Node<E> target = head;
+        Node<E> target = super.head;
         while (true) {
             Node<E> next = target.getNext();
             if (next == null) { break; }
@@ -15,5 +15,26 @@ public class MySortedSet<E extends Comparable<E>> extends MyLinkedList<E> {
 
         target.insertNext(elem);
         super.numItems += 1;
+    }
+
+    //
+    // Returns `true` if deletion took place.
+    // Otherwise, returns `false`
+    //
+    public boolean delete(E elem) {
+        Node<E> prev_target = super.head;
+        while (true) {
+            Node<E> next = prev_target.getNext();
+            if (next == null) { return false; }
+
+            // 일치하는 원소를 찾음
+            if (elem.compareTo(next.item) == 0) { break; }
+
+            prev_target = next;
+        }
+
+        prev_target.removeNext();
+        super.numItems -= 1;
+        return true;
     }
 }
