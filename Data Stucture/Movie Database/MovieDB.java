8159f50c5fb5b080f300a4d06c2a93020c9e73ab
Hyeon Kim <simnalamburt@gmail.com>
2016-04-15 21:39:37 +0900

    불필요한 코드들, 디버그 메세지 삭제

--- Data Stucture/Movie Database/MovieDB.java
+++ Data Stucture/Movie Database/MovieDB.java
@@ -10,61 +10,15 @@ import java.util.NoSuchElementException;
 public class MovieDB {
     private final MySortedSet<MovieDBItem> set = new MySortedSet<MovieDBItem>();
 
-    public void insert(MovieDBItem item) {
-        // TODO: Remove debug codes
-        System.err.printf("[trace] MovieDB: INSERT [%s] [%s]\n", item.genre, item.title);
-
-        set.add(item);
-    }
-
-    public void delete(MovieDBItem item) {
-        // TODO: Remove debug codes
-        System.err.printf("[trace] MovieDB: DELETE [%s] [%s]\n", item.genre, item.title);
-
-        set.delete(item);
-    }
+    public void insert(MovieDBItem item) { set.add(item); }
+    public void delete(MovieDBItem item) { set.delete(item); }
+    public MyLinkedList<MovieDBItem> items() { return set; }
 
     public MyLinkedList<MovieDBItem> search(String term) {
-        // TODO: Remove debug codes
-        System.err.printf("[trace] MovieDB: SEARCH [%s]\n", term);
-
         MyLinkedList<MovieDBItem> result = new MyLinkedList<MovieDBItem>();
         for (MovieDBItem item : set) {
             if (item.title.contains(term)) { result.add(item); }
         }
         return result;
     }
-
-    public MyLinkedList<MovieDBItem> items() {
-        // TODO: Remove debug codes
-        System.err.printf("[trace] MovieDB: ITEMS\n");
-
-        return set;
-    }
 }
-
-// TODO: 이 타입이 필요한가?
-class Genre extends Node<String> implements Comparable<Genre> {
-    public Genre(String name) {
-        super(name);
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    @Override
-    public int compareTo(Genre o) {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    @Override
-    public int hashCode() {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-}
-
-// TODO: 이 타입이 필요한가?
-class MovieList extends MySortedSet<String> { }
