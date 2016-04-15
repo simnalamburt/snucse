fbae4cae02e9676e1c72a832bbbe42de4a50965b
Hyeon Kim <simnalamburt@gmail.com>
2016-04-15 18:40:30 +0900

    MovieDBItem 클래스 구현

--- Data Stucture/Movie Database/MovieDBItem.java
+++ Data Stucture/Movie Database/MovieDBItem.java
@@ -1,53 +1,48 @@
-
-/******************************************************************************
- * MovieDB의 인터페이스에서 공통으로 사용하는 클래스.
- */
+//
+// MovieDB의 인터페이스에서 공통으로 사용하는 클래스
+//
 public class MovieDBItem implements Comparable<MovieDBItem> {
-
-    private final String genre;
-    private final String title;
+    private final String genre, title;
 
     public MovieDBItem(String genre, String title) {
-        if (genre == null) throw new NullPointerException("genre");
-        if (title == null) throw new NullPointerException("title");
+        if (genre == null) { throw new NullPointerException("`genre` should not be null"); }
+        if (title == null) { throw new NullPointerException("`title` should not be null"); }
 
         this.genre = genre;
         this.title = title;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getTitle() {
-        return title;
-    }
+    public String getGenre() { return genre; }
+    public String getTitle() { return title; }
 
     @Override
     public int compareTo(MovieDBItem other) {
-        // TODO delete the line below and implement this method
-        throw new UnsupportedOperationException();
+        int ret = genre.compareTo(other.genre);
+        if (ret != 0) { return ret; }
+
+        return title.compareTo(other.title);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) { return true; }
+        if (obj == null) { return false; }
+        if (getClass() != obj.getClass()) { return false; }
+
         MovieDBItem other = (MovieDBItem) obj;
+
         if (genre == null) {
-            if (other.genre != null)
-                return false;
-        } else if (!genre.equals(other.genre))
-            return false;
+            if (other.genre != null) { return false; }
+        } else {
+            if (!genre.equals(other.genre)) { return false; }
+        }
+
         if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
+            if (other.title != null) { return false; }
+        } else {
+            if (!title.equals(other.title)) { return false; }
+        }
+
         return true;
     }
 
@@ -59,5 +54,4 @@ public class MovieDBItem implements Comparable<MovieDBItem> {
         result = prime * result + ((title == null) ? 0 : title.hashCode());
         return result;
     }
-
 }
