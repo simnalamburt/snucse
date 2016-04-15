545173e2574f7deef6ffe440633a8eb62360ae5a
Hyeon Kim <simnalamburt@gmail.com>
2016-04-15 21:49:29 +0900

    Node를 MyLinkedList 의 하위클래스로 넣어버림

--- Data Stucture/Movie Database/MyLinkedList.java
+++ Data Stucture/Movie Database/MyLinkedList.java
@@ -1,9 +1,13 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+//
+// Homebrewed linked list
+//
 public class MyLinkedList<T> implements MyList<T> {
-    // dummy head
+    // Dummy head
     final Node<T> head = new Node<T>(null);
+
     int count;
 
     @Override public boolean isEmpty() { return head.getNext() == null; }
@@ -42,6 +46,49 @@ public class MyLinkedList<T> implements MyList<T> {
     @Override
     public void removeAll() {
         while (head.getNext() != null) { head.removeNext(); }
+    }
+}
+
+//
+// MyLinkedList의 노드가 될 객체
+//
+class Node<T> {
+    public final T item;
+    private Node<T> next;
+
+    // 새 Node<T>를 만든다.
+    public Node(T obj) {
+        this.item = obj;
+        this.next = null;
+    }
+
+    public Node<T> getNext() { return next; }
+
+    // T obj로 새 Node<T>를 만들어, this의 next 자리에 끼워넣는다.
+    public void insertNext(T obj) {
+        Node<T> new_next = new Node<T>(obj);
+        if (this.next == null) { this.next = new_next; return; }
+
+        Node<T> old_next = this.next;
+
+        // Before: this  ------------------->  old_next
+        //
+        // After:  this  ---> new_next  ---->  old_next
+
+        this.next = new_next;
+        new_next.next = old_next;
+    }
+
+    // this다음에 있는 노드를 링크드리스트에서 제거한다.
+    public void removeNext() {
+        if (this.next == null) { throw new NoSuchElementException(); }
+
+        // Before: this ---> this.next ---> new_next
+        //
+        // After:  this ------------------> new_next
+        //                   this.next ---> new_next
+
+        this.next = this.next.next;
     }
 }
 
