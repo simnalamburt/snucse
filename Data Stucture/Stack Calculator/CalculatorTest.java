b5675b2e4b0756a35ca674eba64911aaa5589633
Hyeon Kim <simnalamburt@gmail.com>
2016-05-02 03:13:15 +0900

    EOF 똑바로 처리함
    기존에는 EOF가 들어오면 널포인터익셉션이 떴음

--- Data Stucture/Stack Calculator/CalculatorTest.java
+++ Data Stucture/Stack Calculator/CalculatorTest.java
@@ -1,30 +1,22 @@
 import java.io.*;
 
-public class CalculatorTest
-{
-    public static void main(String args[])
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class CalculatorTest {
+    public static void main(String args[]) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true)
-        {
-            try
-            {
+        while (true) {
+            try {
                 String input = br.readLine();
-                if (input.compareTo("q") == 0)
-                    break;
+                if (input == null || input.compareTo("q") == 0) { return; }
 
                 command(input);
-            }
-            catch (Exception e)
-            {
-                System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
+            } catch (Exception e) {
+                System.err.println("입력이 잘못되었습니다. 오류 : " + e.toString());
             }
         }
     }
 
-    private static void command(String input)
-    {
+    private static void command(String input) {
         // TODO : 아래 문장을 삭제하고 구현해라.
         System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
     }
