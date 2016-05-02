290547829f6009f6a06927b3fe876fe91c5da4ea
Hyeon Kim <simnalamburt@gmail.com>
2016-05-02 15:27:55 +0900

    Rename: command -> eval

--- Data Stucture/Stack Calculator/CalculatorTest.java
+++ Data Stucture/Stack Calculator/CalculatorTest.java
@@ -13,34 +13,29 @@ public class CalculatorTest {
                 String input = br.readLine();
                 if (input == null || input.compareTo("q") == 0) { return; }
 
-                command(input);
+                System.out.println(eval(input).orElse("ERROR"));
             } catch (Exception e) {
                 System.err.println("입력이 잘못되었습니다. 오류 : " + e.toString());
             }
         }
     }
 
-    private static enum Type { Number, Operator, Whitespace }
-    private static final Lexer<Type> lexer = new Lexer<Type>() {{
-        add("[0-9]+", Type.Number);
-        add("[+\\-*\\/%^()]", Type.Operator);
-        add("[ \t]", Type.Whitespace);
+    private static final Lexer<Parser.Type> lexer = new Lexer<Parser.Type>() {{
+        add("[0-9]+",           Parser.Type.Number);
+        add("[+\\-*\\/%^()]",   Parser.Type.Operator);
+        add("[ \t]",            Parser.Type.Whitespace);
     }};
 
-    private static void command(final String input) {
-        Optional<ArrayList<Token<Type>>> result = lexer.lex(input);
-
-        String msg = result
-            .map((ArrayList<Token<Type>> tokens) ->
-                    tokens.stream()
-                    .filter(token -> token.kind != Type.Whitespace)
-                    .map(token -> token.toString())
-                    .collect(Collectors.joining(" "))
-            )
-            .orElse("ERROR");
-
-        System.out.println(msg);
-
-        // TODO: Implement
+    private static Optional<String> eval(final String input) {
+        return lexer.lex(input)
+            .map(t -> t.stream()
+                .filter(token -> token.kind != Parser.Type.Whitespace)
+                .map(token -> token.toString())
+                .collect(Collectors.joining(" "))
+            );
     }
+}
+
+class Parser {
+    public static enum Type { Number, Operator, Whitespace }
 }
