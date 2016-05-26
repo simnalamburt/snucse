287df94a21e9b84c9d443a4adf543c2a7c04e22f
Hyeon Kim <simnalamburt@gmail.com>
2016-05-27 04:47:58 +0900

    Tab -> Space * 4

--- Data Stucture/Matching/Matching.java
+++ Data Stucture/Matching/Matching.java
@@ -2,30 +2,30 @@ import java.io.*;
 
 public class Matching
 {
-	public static void main(String args[])
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[])
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		while (true)
-		{
-			try
-			{
-				String input = br.readLine();
-				if (input.compareTo("QUIT") == 0)
-					break;
+        while (true)
+        {
+            try
+            {
+                String input = br.readLine();
+                if (input.compareTo("QUIT") == 0)
+                    break;
 
-				command(input);
-			}
-			catch (IOException e)
-			{
-				System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
-			}
-		}
-	}
+                command(input);
+            }
+            catch (IOException e)
+            {
+                System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
+            }
+        }
+    }
 
-	private static void command(String input)
-	{
-		// TODO : 아래 문장을 삭제하고 구현해라.
-		System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
-	}
+    private static void command(String input)
+    {
+        // TODO : 아래 문장을 삭제하고 구현해라.
+        System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
+    }
 }
