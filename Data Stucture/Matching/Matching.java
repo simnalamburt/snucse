eeb80a9351719667fb9bb6c7a89e679489f6df15
Hyeon Kim <simnalamburt@gmail.com>
2016-05-27 04:48:35 +0900

    Misc change

--- Data Stucture/Matching/Matching.java
+++ Data Stucture/Matching/Matching.java
@@ -1,31 +1,23 @@
 import java.io.*;
 
-public class Matching
-{
-    public static void main(String args[])
-    {
+public class Matching {
+    public static void main(String args[]) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true)
-        {
-            try
-            {
+        while (true) {
+            try {
                 String input = br.readLine();
-                if (input.compareTo("QUIT") == 0)
-                    break;
+                if (input.compareTo("QUIT") == 0) { break; }
 
                 command(input);
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
             }
         }
     }
 
-    private static void command(String input)
-    {
-        // TODO : 아래 문장을 삭제하고 구현해라.
+    // TODO: Implement
+    private static void command(String input) {
         System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
     }
 }
