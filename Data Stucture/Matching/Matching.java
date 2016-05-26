05933ce56f688454f0f9c79863aa6183929d305b
Hyeon Kim <simnalamburt@gmail.com>
2016-05-27 08:16:46 +0900

    간단한 BST 생성

--- Data Stucture/Matching/Matching.java
+++ Data Stucture/Matching/Matching.java
@@ -1,5 +1,40 @@
 import java.io.*;
 
+class AVLTree<K extends Comparable<K>, V> {
+    class Node {
+        K key;
+        V value;
+        Node parent, left, right;
+
+        Node(K k, V v) { key = k; value = v; }
+    }
+
+    Node root = null;
+
+    void insert(K key, V value) {
+        // New node which will be inserted
+        Node n = new Node(key, value);
+
+        // Check if initial insertion
+        if (root == null) { root = n; return; }
+
+        // Standard BST insertion
+        Node y, x = root;
+        do {
+            y = x;
+            x = n.key.compareTo(x.key) < 0 ? x.left : x.right;
+        } while (x != null);
+        n.parent = y;
+        if (n.key.compareTo(y.key) < 0) {
+            y.left = n;
+        } else {
+            y.right = n;
+        }
+
+        // TODO: Balancing
+    }
+}
+
 public class Matching {
     public static void main(String args[]) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
