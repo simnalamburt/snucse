c8f4ce01e8c7c8ecbae5679d4175d555a6a03bc9
Hyeon Kim <simnalamburt@gmail.com>
2016-06-10 18:41:47 +0900

    Parse argv

--- Data Stucture/Subway/Subway.java
+++ Data Stucture/Subway/Subway.java
@@ -3,5 +3,12 @@ import java.util.*;
 
 public class Subway {
     public static void main(String args[]) {
+        if (args.length != 1) {
+            System.err.println("\u001B[33m입력이 올바르게 주어지지 않음\u001B[0m");
+            System.exit(1);
+        }
+
+        String filename = args[0];
+        System.out.println(filename);
     }
 }
