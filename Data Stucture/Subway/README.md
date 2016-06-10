d1be4b82571b1ac8c9b6dca9e3931ec71b2af4ec
Hyeon Kim <simnalamburt@gmail.com>
2016-06-10 18:31:06 +0900

    서브웨이 시작

---
+++ Data Stucture/Subway/README.md
@@ -0,0 +1,7 @@
+Subway
+========
+
+```
+make
+java Subway
+```
---
+++ Data Stucture/Subway/Subway.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+public class Subway {
+    public static void main(String args[]) {
+    }
+}
